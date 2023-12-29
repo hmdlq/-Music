56dc9c93135778a8484540d5fe8e885dbb7d5399
hmdlq <153089177+hmdlq@users.noreply.github.com>
2023-12-29 16:13:22 +0300

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Zilzalll/ZThon-Music",
+    "https://github.com/hmdlq/-Music",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
